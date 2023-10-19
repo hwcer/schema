24a6ad1af59aa46bbfd94f58ea5d0e9b55cae17f
万超 黄 <hwclegend@163.com>
2023-10-19 16:37:45 +0800

    dev

--- getter.go
+++ getter.go
@@ -2,6 +2,7 @@ package schema
 
 import (
 	"fmt"
+	"github.com/hwcer/logger"
 	"reflect"
 )
 
@@ -28,7 +29,7 @@ func (schema *Schema) Getter(value reflect.Value, keys ...any) any {
 	case reflect.Slice, reflect.Array:
 		return schema.GetArrayValue(value, keys...)
 	default:
-		fmt.Printf("schema.Getter:子类型不正确无法继续递归\n")
+		logger.Alert("schema.Getter:子类型不正确无法继续递归\n")
 		return nil
 	}
 }
@@ -37,16 +38,31 @@ func (schema *Schema) GetMapValue(value reflect.Value, keys ...any) any {
 	if !value.IsValid() || value.IsNil() {
 		return nil
 	}
-	v := value.MapIndex(reflect.ValueOf(keys[0]))
+	if len(keys) == 0 {
+		return value.Interface()
+	}
+	if value.Kind() != reflect.Map {
+		return nil
+	}
+	t := value.Type().Key()
+	k := ValueOf(keys[0])
+	var v reflect.Value
+
+	if t.AssignableTo(k.Type()) {
+		v = value.MapIndex(k)
+	} else if t.ConvertibleTo(k.Type()) {
+		v = value.MapIndex(k.Convert(t))
+	} else {
+		logger.Alert("schema.GetMapValue:key类型错误\n")
+		return nil
+	}
 	return schema.Getter(v, keys[1:]...)
 
 }
 func (schema *Schema) GetArrayValue(value reflect.Value, keys ...any) any {
-	n := len(keys)
-	if n == 0 || !value.IsValid() {
+	if len(keys) == 0 || !value.IsValid() {
 		return nil
 	}
-
 	i := int(ToInt(keys[0]))
 	if i >= value.Len() {
 		return nil
